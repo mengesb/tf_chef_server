e6180bf50b467cbcc6f5bf9d0fdb3e45f42f15e5
Brian Menges <mengesb@gmail.com>
2016-02-03 11:35:46 -0800

    Removing AMI magic * Getting too complex, simplifying things

--- chef-server-ami/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ami_id" {
-  value = "${lookup(var.amis, format(\"%s-%s-%s\", var.region, var.dist, var.arch))}"
-}
-output "username" {
-  value = "${lookup(var.ami_user, var.dist)}"
-}
-output "packager" {
-  value = "${lookup(var.packager, var.dist)}"
-}
