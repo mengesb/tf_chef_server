5bc32bcfa2d50777e06b85c092e36e55f0a7c00c
Brian Menges <mengesb@gmail.com>
2016-02-04 20:15:00 -0800

    Fixing the chef-server-core install method * removed cookbooks directory file sync * execute chef server creation directly at server * create first user * create first org * associate user to org * download user pem * download org validator pem

--- variables.tf
+++ variables.tf
@@ -1,27 +1,66 @@
 # AWS provider specific configs
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_key_name" {}
-variable "aws_private_key_file" {}
-variable "aws_vpc_id" {}
-variable "aws_subnet_id" {}
-variable "aws_ami_user" {}
+variable "aws_access_key" {
+  description = "Your AWS key (ex. $AWS_ACCESS_KEY_ID)"
+}
+variable "aws_secret_key" {
+  description = "Your AWS secret (ex. $AWS_SECRET_ACCESS_KEY)"
+}
+variable "aws_key_name" {
+  description = "Name of the key pair uploaded to AWS"
+}
+variable "aws_private_key_file" {
+  description = "Full path to your local private key"
+}
+variable "aws_vpc_id" {
+  description = "AWS VPC id (ex. vpc-ffffffff)"
+}
+variable "aws_subnet_id" {
+  description = "AWS Subnet id (ex. subnet-ffffffff)"
+}
+variable "aws_ami_user" {
+  description = "AWS AMI default username"
+}
 variable "aws_ami_id" {
+  description = "AWS Instance ID (region dependent)"
   default = "ami-45844401"
 }
 variable "aws_flavor" {
+  description = "AWS Instance type to deploy"
   default = "c3.xlarge"
 }
 variable "aws_region" {
+  description = "AWS Region to deploy to"
   default = "us-west-1"
 }
 variable "aws_instance_name" {
   default = "chef-server"
 }
 variable "aws_instance_count" {
+  description = "Number of CHEF Servers to provision. DO NOT CHANGE!"
   default = 1
 }
 # tf_chef_server specific configs
 variable "chef_org" {
+  description = "Short CHEF Server organization name (lowercase alphanumeric characters only)"
   default = "example"
+}
+variable "chef_org_long" {
+  description = "Long form name of your first CHEF Server organization"
+  default = "Example CHEF Organization"
+}
+variable "chef_username" {
+  description = "Username of the first CHEF Server user"
+  default = "example"
+}
+variable "chef_user_firstname" {
+  description = "CHEF Server user's first name"
+  default = "example"
+}
+variable "chef_user_lastname" {
+  description = "CHEF Server user's last name"
+  default = "user"
+}
+variable "chef_user_email" {
+  description = "CHEF Server user's e-mail"
+  default = "example@domain.tld"
 }
