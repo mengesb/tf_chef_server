5a2d1cca62ccb24a744e12361cccfaf603869207
Brian Menges <mengesb@gmail.com>
2016-02-04 21:47:31 -0800

    Removed cookbooks

--- cookbooks/chef-ingredient/test/integration/chefdk/serverspec/test_chefdk_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'test::chefdk' do
-  it 'chefdk should be version 0.7.0' do
-    command = `/opt/chefdk/bin/chef --version`
-    expect(command).to include('Chef Development Kit Version: 0.7.0')
-  end
-end
